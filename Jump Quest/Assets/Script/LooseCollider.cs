8e9771767ed35ba867b00426b4d705dab61ab189
Furbet <38845441+furbet001@users.noreply.github.com>
2018-04-30 21:38:30 +1000

    Timer, Collider, Flag

---
+++ Jump Quest/Assets/Script/LooseCollider.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LooseCollider : MonoBehaviour {
+private LevelManager levelManager; 
+	
+	
+	void Start(){
+	
+	
+	
+	
+	}
+	
+	
+	void OnCollisionEnter2D(Collision2D collision){
+		levelManager = GameObject.FindObjectOfType<LevelManager> (); 
+	levelManager.LoadLevel ("Lose");
+		print ("Collision");
+	}
+		
+						
+	}
+	
+	
+
