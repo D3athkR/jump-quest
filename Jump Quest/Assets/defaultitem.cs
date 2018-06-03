04d1eb7e456c2fb66c323e9050faa59ba7103edd
Furbet <38845441+furbet001@users.noreply.github.com>
2018-06-03 17:15:50 +1000

    health update

---
+++ Jump Quest/Assets/defaultitem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class defaultitem : MonoBehaviour {
+
+void OnCollisionEnter2D(Collision2D collision){
+
+	var hit = collision.gameObject;
+	var health = hit.GetComponent<health>(); 
+	if (health != null)
+	{
+	health.TakeDamage(10);
+
+		
+	}
+}
+}
+
---
+++ Jump Quest/Assets/health.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class health : MonoBehaviour {
+public const int maxHealth = 100;
+public int currentHealth= maxHealth;
+public RectTransform healthBar;
+
+
+public void TakeDamage (int amount)
+{
+
+	currentHealth -= amount;
+	if (currentHealth <=0)
+	{
+	currentHealth = 0;
+	Debug.Log ("Dead!"); 
+	Destroy(gameObject);
+		}
+
+   if (currentHealth <= 50)
+   {
+   Debug.Log ("half reduced");
+
+  }
+
+		healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
+
+}
+}
+
