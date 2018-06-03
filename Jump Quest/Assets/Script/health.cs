2fbea439a7e69f25f9fae2d17cb3cd61f23b8202
Willeeh <master_intelligent@hotmail.com>
2018-06-03 21:45:13 +1000

    Speed&jump reduction
    When health is <=50, maxspeed and jumpforce is reduced.

--- Jump Quest/Assets/Script/health.cs
+++ Jump Quest/Assets/Script/health.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
+using UnitySampleAssets._2D; //uses scripts from Unity Asset 2D Folder
 
 public class health : MonoBehaviour {
 public const int maxHealth = 100;
 public int currentHealth= maxHealth;
 public RectTransform healthBar;
+
+
+void start(){
+}
 
 
 public void TakeDamage (int amount)
@@ -23,7 +28,10 @@ public void TakeDamage (int amount)
    if (currentHealth <= 50)
    {
    Debug.Log ("half reduced");
-
+	GameObject thePlayer = GameObject.Find("Player1");
+	PlatformerCharacter2D platform = thePlayer.GetComponent<PlatformerCharacter2D>();
+	platform.maxSpeed = 1f;
+	platform.jumpForce = 200f;
   }
 
 		healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
