80296613c825b9ae0cd784d1d0882ffd0589ca26
Furbet <38845441+furbet001@users.noreply.github.com>
2018-06-04 16:19:46 +1000

    new update

---
+++ Jump Quest/Assets/randxspawn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randxspawn : MonoBehaviour {
+public GameObject enemy; 
+float randX;
+Vector2 wheretoSpawn;
+public float Spawnrate= 2f;
+float nextSpawn= 0.0f;
+
+	void Update () {
+
+	if (Time.time > nextSpawn)
+	{
+	nextSpawn = Time.time +Spawnrate;
+	randX = Random.Range(-16.55f,-1.94f);
+	wheretoSpawn= new Vector2 (randX,transform.position.y);
+	Instantiate (enemy, wheretoSpawn, Quaternion.identity);
+
+				
+	}
+}
+}
