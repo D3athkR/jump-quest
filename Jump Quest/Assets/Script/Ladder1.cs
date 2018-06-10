604858ba8c6e9a7533de9b8bbe8a0af078c131e9
Willeeh <master_intelligent@hotmail.com>
2018-06-11 01:21:34 +1000

    Added Ropes

---
+++ Jump Quest/Assets/Script/Ladder1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ladder1 : MonoBehaviour {
+
+	public float speed1=4; 
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerStay2D(Collider2D Other)
+	{
+		if (Other.tag=="Player"&&Input.GetKey(KeyCode.RightArrow)){
+			Other.GetComponent<Rigidbody2D>().velocity=new Vector2(speed1,0);
+			}
+		else if (Other.tag=="Player"&&Input.GetKey(KeyCode.LeftArrow)){
+			Other.GetComponent<Rigidbody2D>().velocity=new Vector2(-speed1,0);
+			}
+		else{
+			Other.GetComponent<Rigidbody2D>().velocity=new Vector2(2f,0.6f);
+			}
+	}
+}
