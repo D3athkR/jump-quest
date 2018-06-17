51790e72d817643dec3f824af9bd84a873f389b9
Furbet <38845441+furbet001@users.noreply.github.com>
2018-06-17 13:12:17 +1000

    new update

---
+++ Jump Quest/Assets/sink.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sink : MonoBehaviour {
+private LevelManager levelmanager; 
+
+
+	void OnCollisionEnter2D(Collision2D collision) 
+{
+		levelmanager = GameObject.FindObjectOfType<LevelManager> (); 
+		levelmanager.LoadLevel ("sink");
+		print ("Collision");
+		GameObject.Find ("Player1").SendMessage ("Finish"); 
+}
+}
