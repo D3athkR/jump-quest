dbe232a521767fe7041bb2ca3be25a8181d0533a
Willeeh <master_intelligent@hotmail.com>
2018-06-29 00:25:24 +1000

    ZoomButton Added

---
+++ Jump Quest/Assets/Script/ZoomCamera1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class ZoomCamera1 : MonoBehaviour {
+
+	public float zoom = 120f;
+	public float smooth =5;
+	//public float normal = 60f; replace 'Camera.current.fieldOfView' with 'normal' to preset normal camera view 
+	//reference tutorial - https://www.youtube.com/watch?v=-nKn7l1iu2g 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(CrossPlatformInputManager.GetButton("Zoom")) {
+			GetComponent<Camera>().fieldOfView = Mathf.Lerp(GetComponent<Camera>().fieldOfView, zoom, Time.deltaTime * smooth); 
+		}
+		else {
+			GetComponent<Camera>().fieldOfView = Mathf.Lerp(GetComponent<Camera>().fieldOfView, Camera.current.fieldOfView, Time.deltaTime * smooth); 
+			}
+	}
+}
+   
